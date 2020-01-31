2c3991906b4048720d3686b9e7e52cba82eedf6c
Srdjan Jokic <jokic.srdjan01@gmail.com>
2020-01-31 21:40:11 +0100

    Player movement and camera.

---
+++ GGJ2020/Assets/Scripts/PlayerLook.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+
+public class PlayerLook : MonoBehaviour
+{
+    [SerializeField] private Transform playerBody = null;
+    [SerializeField] private Vector3 camPositionOnPlayer = Vector3.zero;
+    [SerializeField] private float mouseSensitivity = 100f;
+
+    private float xRot = 0f;
+
+    private void OnEnable()
+    {
+        transform.parent = playerBody;
+        transform.localPosition = camPositionOnPlayer;
+    }
+
+    private void Update()
+    {
+        float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
+        float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime;
+
+        xRot -= mouseY;
+        xRot = Mathf.Clamp(xRot, -90f, 90f);
+
+        transform.localRotation = Quaternion.Euler(xRot, 0f, 0f);
+        playerBody.Rotate(Vector3.up * mouseX);
+    }
+}
---
+++ GGJ2020/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField] private Transform groundCheck = null;
+    [SerializeField] private LayerMask groundMask = 0;
+    [SerializeField] private float groundDistance = 0f;
+    [SerializeField] private float playerSpeed = 12f;
+    [SerializeField] private float jumpHeight = 10f;
+
+    private CharacterController controller;
+    private Vector3 jumpVelocity;
+
+    private void Awake()
+    {
+        controller = GetComponent<CharacterController>();
+    }
+
+    private void Update()
+    {
+        HandleMovement();
+        HandleJump();
+        ApplyGravity();
+    }
+
+    private void HandleMovement()
+    {
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
+
+        Vector3 moveDirection = transform.right * x + transform.forward * z;
+        moveDirection = Vector3.ClampMagnitude(moveDirection, 1.0f);
+
+        Vector3 movement = moveDirection * playerSpeed * Time.deltaTime;
+
+        controller.Move(movement);
+    }
+
+    private void HandleJump()
+    {
+        if (Input.GetButtonDown("Jump") && IsGrounded())
+        {
+            // v = sqrt(height * -2 * gravity) => velocity needed to jump a certain height
+            jumpVelocity.y = Mathf.Sqrt(jumpHeight * -2f * Physics.gravity.y);
+
+            //AudioManager.instance.PlaySound((int)AudioManager.Sounds.Jump);
+        }
+    }
+
+    private void ApplyGravity()
+    {
+        if(jumpVelocity.y > Physics.gravity.y)
+        {
+            jumpVelocity += Physics.gravity * Time.deltaTime;
+        }
+
+        controller.Move(jumpVelocity * Time.deltaTime);
+    }
+
+    private bool IsGrounded()
+    {
+        return Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
+    }
+}
