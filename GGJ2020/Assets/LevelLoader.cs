98a71faba496c224d81afb49a10bb20f8d522d01
NSKolazar <nathansears.k@gmail.com>
2020-02-01 20:08:47 +0100

    Loading Screen.

---
+++ GGJ2020/Assets/LevelLoader.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class LevelLoader : MonoBehaviour
+{
+    public GameObject loadingScreen;
+    public Slider slider;
+    public void LevelLoad(int sceneIndex)
+    {
+        StartCoroutine(LoadAsync(sceneIndex));   
+    }
+
+    IEnumerator LoadAsync(int sceneIndex)
+    {
+        AsyncOperation loading = SceneManager.LoadSceneAsync(sceneIndex);
+
+        loadingScreen.SetActive(true);
+
+        while(!loading.isDone)
+        {
+            float progress = Mathf.Clamp01(loading.progress / 0.9f);
+            slider.value = progress;
+            yield return null;
+        }
+    }
+    
+}
---
+++ GGJ2020/Assets/Timer.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class Timer : MonoBehaviour
+{
+    public Text timer;
+
+    private void Start()
+    {
+        StartCoroutine(Countdown());
+    }
+    public IEnumerator Countdown()
+    {
+        float timeLeft = 10;
+        timer = GetComponent<Text>();
+        while (timeLeft > 0)
+        {
+            timer.text = "Time: " + timeLeft-- + "s";
+           
+            yield return new WaitForSecondsRealtime(1);
+        }
+        if (timeLeft == 0)
+        {
+            timer.text = "Time's up!";
+        }
+        
+    }
+}
